87e3c1df740a15400c388dd4a28821bb48ce2a93
jungshen <979692441@qq.com>
2019-01-10 19:22:57 +0800

    '.'

---
+++ src/Client.php
@@ -0,0 +1,15 @@
+<?php
+namespace jungshen\test;
+/**
+ * Created by PhpStorm.
+ * User: jungshen
+ * Date: 2019/1/10
+ * Time: 19:04
+ */
+class Client
+{
+    static function hello()
+    {
+        echo 'Hello jungshen';
+    }
+}
--- src/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jungshen
- * Date: 2019/1/10
- * Time: 19:04
- */
-echo 'HELLO Jungshen';
